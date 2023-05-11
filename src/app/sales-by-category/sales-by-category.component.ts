c507948c8968872b2e7d0b857a96d305d15cfb24
adem saidani <ademsaidani799@gmail.com>
2023-05-11 14:54:08 +0200

    adding last few transction table

--- src/app/sales-by-category/sales-by-category.component.ts
+++ src/app/sales-by-category/sales-by-category.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Chart } from 'angular-highcharts';
 
 @Component({
   selector: 'app-sales-by-category',
@@ -6,5 +7,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./sales-by-category.component.css']
 })
 export class SalesByCategoryComponent {
+
+  chart = new Chart({
+    chart: {
+      type: 'pie',
+      height: 325
+    },
+    title: {
+      text: 'Category wise sales'
+    },
+    xAxis: {
+      categories: [
+        'Electronics',
+        'Groceries',
+        'Cosmetics',
+        'Clothes',
+        'Appliances',
+      ]
+    },
+    yAxis: {
+      title: {
+        text: 'Revenue in %'
+      }
+    },
+    series: [
+     {
+      type: 'pie',
+      data: [
+        {
+          name: 'Electronics',
+          y: 41.0,
+          color: '#044342',
+        },
+        {
+          name: 'Groceries',
+          y: 33.8,
+          color: '#7e0505',
+        },
+        {
+          name: 'Cosmetics',
+          y: 6.5,
+          color: '#ed9e20',
+        },
+        {
+          name: 'Clothes',
+          y: 15.2,
+          color: '#6920fb',
+        },
+        {
+          name: 'Appliances',
+          y: 3.5,
+          color: '#121212',
+        },
+      ]
+     }
+    ],
+    credits: {
+      enabled: false
+    }
+  })
+
+
 
 }
